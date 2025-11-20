f74286d49c549e24faaa7a9a37a6ce53dde31864
Kiritxxx <pcnine550@gmail.com>
2025-11-19 19:16:21 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CybersecurityDiagnostic from './components/CybersecurityDiagnostic';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CybersecurityDiagnostic />
+    </div>
+  );
+}
+
+export default App;
